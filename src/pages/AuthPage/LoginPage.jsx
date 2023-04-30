2c0da71a2fd36ae29cd09427c4061c6bc64ec9d5
Alex <alex.gromov34502@gmail.com>
2023-04-30 17:15:22 +0300

    Изменен компонент модального окна, добавлены иллюстрации на страницы авторизации

--- src/pages/AuthPage/LoginPage.jsx
+++ src/pages/AuthPage/LoginPage.jsx
@@ -5,6 +5,8 @@ import Button from "../../components/Button/Button";
 
 import styles from "./AuthPage.module.scss";
 
+import illustration from "../../assets/images/loginIllustrayion.png";
+
 function LoginPage() {
 	function login(e) {
 		e.preventDefault();
@@ -13,33 +15,39 @@ function LoginPage() {
 
 	return (
 		<Modal dialogClassName={styles.dialog}>
-			<form className={styles.form} onSubmit={login}>
-				<h1 className={styles.formHeader}>Вход</h1>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Email:"
-						inputType="email"
-						inputPlaceholder="Введите email"
-					/>
-				</div>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Пароль:"
-						inputType="password"
-						inputPlaceholder="Введите пароль"
-					/>
-					<Link className={styles.formSecondary} to="/password-reset">
-						Забыли пароль?
+			<div className={styles.container}>
+				<form className={styles.form} onSubmit={login}>
+					<h1 className={styles.formHeader}>Вход</h1>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Email:"
+							inputType="email"
+							inputPlaceholder="Введите email"
+						/>
+					</div>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Пароль:"
+							inputType="password"
+							inputPlaceholder="Введите пароль"
+						/>
+						<Link
+							className={styles.formSecondary}
+							to="/password-reset"
+						>
+							Забыли пароль?
+						</Link>
+					</div>
+					<Button text="Войти" />
+					<Link
+						className={`${styles.formSecondary} ${styles.formSecondaryReg}`}
+						to="/register"
+					>
+						Регистрация
 					</Link>
-				</div>
-				<Button text="Войти" />
-				<Link
-					className={`${styles.formSecondary} ${styles.formSecondaryReg}`}
-					to="/register"
-				>
-					Регистрация
-				</Link>
-			</form>
+				</form>
+				<img src={illustration} alt="" />
+			</div>
 		</Modal>
 	);
 }
--- src/pages/AuthPage/RegisterPage.jsx
+++ src/pages/AuthPage/RegisterPage.jsx
@@ -5,58 +5,63 @@ import { Link } from "react-router-dom";
 
 import styles from "./AuthPage.module.scss";
 
+import illustration from "../../assets/images/registrationIllustration.png";
+
 function RegisterPage() {
 	function register(e) {
 		e.preventDefault();
-		console.log('регистрация');
+		console.log("регистрация");
 	}
 
 	return (
 		<Modal>
-			<form className={styles.form} onSubmit={register}>
-				<h1 className={styles.formHeader}>Регистрация</h1>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Email:"
-						inputType="email"
-						inputPlaceholder="Введите email"
-					/>
-				</div>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Никнейм:"
-						inputType="text"
-						inputPlaceholder="Введите никнейм"
-					/>
-				</div>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Дата рождения:"
-						inputType="date"
-						inputPlaceholder="12.12.2000"
-						onFocus={(e) => {
-							e.currentTarget.type = "date";
-						}}
-						onBlur={(e) => {
-							e.currentTarget.type = "text";
-						}}
-					/>
-				</div>
-				<div className={styles.formElement}>
-					<LabelInput
-						label="Пароль:"
-						inputType="password"
-						inputPlaceholder="Введите пароль"
-					/>
-				</div>
-				<Button text="Зарегистрироваться" />
-				<Link
-					className={`${styles.formSecondary} ${styles.formSecondaryReg}`}
-					to="/login"
-				>
-					Вход
-				</Link>
-			</form>
+			<div className={styles.container}>
+				<form className={styles.form} onSubmit={register}>
+					<h1 className={styles.formHeader}>Регистрация</h1>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Email:"
+							inputType="email"
+							inputPlaceholder="Введите email"
+						/>
+					</div>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Никнейм:"
+							inputType="text"
+							inputPlaceholder="Введите никнейм"
+						/>
+					</div>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Дата рождения:"
+							inputType="text"
+							inputPlaceholder="12.12.2000"
+							onFocus={(e) => {
+								e.currentTarget.type = "date";
+							}}
+							onBlur={(e) => {
+								e.currentTarget.type = "text";
+							}}
+						/>
+					</div>
+					<div className={styles.formElement}>
+						<LabelInput
+							label="Пароль:"
+							inputType="password"
+							inputPlaceholder="Введите пароль"
+						/>
+					</div>
+					<Button text="Зарегистрироваться" />
+					<Link
+						className={`${styles.formSecondary} ${styles.formSecondaryReg}`}
+						to="/login"
+					>
+						Вход
+					</Link>
+				</form>
+				<img className={styles.illustration} src={illustration} alt="running man illustration" />
+			</div>
 		</Modal>
 	);
 }
