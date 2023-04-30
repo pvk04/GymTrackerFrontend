f0fa5af4cad6ade3f6a8721e44eeb3604e2d4e40
Alex <alex.gromov34502@gmail.com>
2023-04-30 18:21:04 +0300

    Доверстан AsideBar

--- src/components/AsideBar/AsideBar.jsx
+++ src/components/AsideBar/AsideBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
+import styles from "./AsideBar.module.scss";
+
+import logo from "../../assets/images/logo.png";
 import profile from "../../assets/images/profile.svg";
 import workout from "../../assets/images/workout.svg";
 import exercise from "../../assets/images/exercise.svg";
 import suplement from "../../assets/images/suplement.svg";
-
-import styles from "./AsideBar.module.scss";
+import logout from "../../assets/images/logout.svg";
 
 const MenuItems = [
 	{ name: "Профиль", href: "/profile", icon: profile },
@@ -21,8 +24,8 @@ const MenuItems = [
 function AsideBar() {
 	return (
 		<aside className={styles.aside}>
-			<div>
-				<img src="" alt="logo" />
+			<div className={styles.logo}>
+				<img src={logo} alt="logo" />
 				<h1>GymTracker</h1>
 			</div>
 			<nav className={styles.nav}>
@@ -42,7 +45,10 @@ function AsideBar() {
 					))}
 				</ul>
 			</nav>
-			<button>Выход</button>
+			<button className={styles.logout}>
+				<img src={logout} alt="" />
+				<span>Выход</span>
+			</button>
 		</aside>
 	);
 }
